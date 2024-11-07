3ed31c0fc1783245604f9f93ed52f45e908ae45f
mariazinha56 <143208855+mariazinha56@users.noreply.github.com>
2024-11-07 08:34:11 -0300

    ASDGSDF

--- pagina2.js
+++ pagina2.js
@@ -3,7 +3,7 @@ function criaCartao (categoria,pergunta,resposta){
     let cartao=document.createElement('article');
     cartao.className='cartao';
     cartao.innerHTML=`
-    <div class="conteudo-cartao">
+    <div class="conteudo-cartao" onclick ="mostraResposta(card)">
    <h3>${categoria}</h3>
    <div class="pergunta-cartao">
        <p>${pergunta}</p>
@@ -13,7 +13,15 @@ function criaCartao (categoria,pergunta,resposta){
    </div>
 </div>
     `
-
     
 container.appendChild(cartao);
+}
+function mostraResposta(card){
+  const resposta=card.querySelector('.resposta-cartao');
+  if(resposta.style.display ==='none'){
+     resposta.style.display = 'block'
+  }
+  else{
+     resposta.style.display = 'none'
+  }
 }
